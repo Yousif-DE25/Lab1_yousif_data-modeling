46ae66c15cbf8a84b92480357d6d61e420763422
Yousif Alsaedi <yousifalsaedi90@yahoo.com>
2026-01-28 23:50:09 +0100

    Klar

--- yh_labb/insert_data.sql
+++ yh_labb/insert_data.sql
@@ -9,11 +9,15 @@ VALUES ('Ignore', 'Dad');
 
 -- Klass
 INSERT INTO klass (klassnamn, startdatum, program_id, utbildningsledare_id)
-VALUES ('DE23', '2023-09-01', 6, 7);
+VALUES 
+('DE23', '2023-09-01', 6, 7),
+('DE24', '2024-09-01', 6, 7),
+('DE25', '2025-09-01', 6, 7);
 
 -- Student
 INSERT INTO student (förnamn, efternamn, email, klass_id)
-VALUES ('Gustaf', 'Mohammedsson', 'Gushamm@gmail.com', 1),
+VALUES 
+('Gustaf', 'Mohammedsson', 'Gushamm@gmail.com', 1),
 ('Lisa', 'Ann', 'Hub@hotmail.com', 1);
 
 -- Utbildare
@@ -28,3 +32,10 @@ VALUES ('Data modellering', 'DE23_145', 40, 'Grundläggande data modellering', 9
 INSERT INTO företag (företagsnamn, organisationsnummer, har_f_skatt, adress)
 VALUES ('Scania AB', '676767-6677', True, 'Stockholm');
 
+-- Konsult
+INSERT INTO konsult (utbildare_id, företag_id)
+VALUES(1,1);
+
+-- Kurs_Utbildare
+INSERT INTO kurs_utbildare (kurs_id, utbildare_id)
+VALUES(1,1);
